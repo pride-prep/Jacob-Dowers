7e4ef51519798632c98539fb307decba5ca4ec0a
Jacob <jacob@my.prideprepschool.org>
2017-04-26 13:15:28 -0700

    i started HW1

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Student on 4/26/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func magicbutton(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var changelabel: UILabel!
+    @IBOutlet weak var type: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
