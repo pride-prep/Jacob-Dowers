a58789bc778dd2c8d6650f387fddf27119591dd7
Jacob <jacob@my.prideprepschool.org>
2017-05-08 13:16:49 -0700

    HW2 stuffffffff

--- HW2/HW2/ViewController.swift
+++ HW2/HW2/ViewController.swift
@@ -49,6 +49,34 @@ class ViewController: UIViewController {
         totalprice=0
         total.text="$\(totalprice)"
     }
+    @IBAction func apple2(_ sender: Any) {
+        totalprice=totalprice-1
+        total.text="$\(totalprice)"
+    }
+    @IBAction func steak2(_ sender: Any) {
+        totalprice=totalprice-9
+        total.text="$\(totalprice)"
+    }
+    @IBAction func chicken2(_ sender: Any) {
+        totalprice=totalprice-5
+        total.text="$\(totalprice)"
+    }
+    @IBAction func banana2(_ sender: Any) {
+        totalprice=totalprice-2
+        total.text="$\(totalprice)"
+    }
+    @IBAction func bread2(_ sender: Any) {
+        totalprice=totalprice-3
+        total.text="$\(totalprice)"
+    }
+    @IBAction func ice_cream2(_ sender: Any) {
+        totalprice=totalprice-7
+        total.text="$\(totalprice)"
+    }
+    @IBAction func yogurt2(_ sender: Any) {
+        totalprice=totalprice-4
+        total.text="$\(totalprice)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
